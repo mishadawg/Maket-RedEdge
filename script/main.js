23add6299200a9caa681faad1ea5198c8bc4cb17
Micha <mi>
2021-03-09 16:40:08 +0300

    11.2all files update

--- script/main.js
+++ script/main.js
@@ -1,1 +1,25 @@
-// carusel Js
+// carusel
+let arrOfPag = document.getElementsByClassName('pagNavSelf');
+let arrOfNews = document.getElementsByClassName('newsSelf');
+// install
+for (let i = 0; i < arrOfPag.length; i++) {
+    arrOfPag[i].addEventListener('click',carusel,false);
+}
+// functions
+function carusel(e) {
+    selection(e);
+}
+function deletAllclasses() {
+    for (let l = 0; l < arrOfPag.length; l++) { 
+        arrOfPag[l].classList.remove('selected');
+    }
+}
+function selection(e) {
+    let selected = e.target.classList.contains('selected');
+    if(selected == true){
+        e.target.classList.remove('selected');
+    }else if(selected == false){
+        deletAllclasses();
+        e.target.classList.add('selected');
+    }
+}
