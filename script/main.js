7a7d7b8b009102c86727064c4ce61a7b2b27c58c
Micha <mi>
2021-03-10 14:13:00 +0300

    12.3 нужно доделать меню бургер.
    Добавил меню бургер и теперь его нужно отредактировать согласно макету.

--- script/main.js
+++ script/main.js
@@ -14,14 +14,55 @@ function carusel(e) {
     selection(e);
     checkSelect();
 }
+function width() {
+    return document.body.clientWidth;
+}
+function widthChange(slide) {
+    switch (slide) {
+    case 1:
+        if(width() == 1440){
+            caruselSelf.style.marginLeft = '0px';
+        }else if(width() == 1024){
+            caruselSelf.style.marginLeft = '0px';
+        }else if(width() == 768){
+            caruselSelf.style.marginLeft = '0px';
+        }else{
+            caruselSelf.style.marginLeft = '0px';
+        }
+    break;
+    case 2:
+        if(width() == 1440){
+            caruselSelf.style.marginLeft = '-1145px';
+        }else if(width() == 1024){
+            caruselSelf.style.marginLeft = '-980px';
+        }else if(width() == 768){
+            caruselSelf.style.marginLeft = '-730px';
+        }else{
+            caruselSelf.style.marginLeft = '-1145px';
+        }
+    break;
+    case 3:
+        if(width() == 1440){
+            caruselSelf.style.marginLeft = '-2315px';
+        }else if(width() == 1024){
+            caruselSelf.style.marginLeft = '-1985px';
+        }else if(width() == 768){
+            caruselSelf.style.marginLeft = '-1480px';
+        }else{
+            caruselSelf.style.marginLeft = '-2315px';
+        }
+    break;      
+    }
+   
+}
 // additional func
 function checkSelect() {
     if (arrOfPag[0].classList.contains('selected') == true){
-        caruselSelf.style.marginLeft = '0px';
+        widthChange(1)
     }else if (arrOfPag[1].classList.contains('selected') == true){
-        caruselSelf.style.marginLeft = '-1145px';
+        widthChange(2)
     }else if (arrOfPag[2].classList.contains('selected') == true){
-        caruselSelf.style.marginLeft = '-2315px';
+        widthChange(3)
     }  
 }
 function deletAllclasses() {
